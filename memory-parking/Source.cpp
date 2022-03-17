beb490c5162c8896e332af7a6a8a94dc4f0136be
luci <radu4cristian1@gmail.com>
2022-03-17 20:12:57 +0200

    daaa

--- memory-parking/Source.cpp
+++ memory-parking/Source.cpp
@@ -1,14 +1,12 @@
 #include <filesystem>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/features2d.hpp>
-#include <opencv2/xfeatures2d.hpp>
 #include <iostream>
 #include <string>
 #include <cstring>
 #include <fstream>
 #include <list>
+#include "Utils.h"
+#pragma warning( disable : 91)
+
 #define SIZE_OF_PHOTO 200;
 using namespace cv;
 using namespace cv::xfeatures2d;
@@ -25,6 +23,14 @@ string* ReadFile(string dir) {
 	}
 	return photos;
 }
+void __cxa_allocate_exception() { abort(); }
+void __cxa_throw() { abort(); }
+Mat PercentOnImage(Mat img, float percent) {
+
+	putText(img, "Percent : "+to_string(percent)+"%", Point(10, img.rows / 2), FONT_HERSHEY_DUPLEX, 1.0, CV_RGB(118, 185, 0), 2);
+
+	return img;
+}
 
 int main()
 {
@@ -32,14 +38,15 @@ int main()
 	string* imgs = ReadFile(path1);
 	const String path2 = "vid/";
 	string* vid = ReadFile(path2);
-	
+	int nr = 0;
+	string nrs;
 
 
 	//Images
-	for(int i= 0; i< 8; i++){
-		Mat img = imread(path1+imgs[i]+".jpg", IMREAD_GRAYSCALE);
+	for(int i= 0; i< 3; i++){
+		Mat img = imread(path1+imgs[i]+".jpg");
 
-		resize(img, img, cv::Size(300, 300), 0.75, 0.75);
+		resize(img, img,Size(300, 300), 0.75, 0.75);
 		if (img.empty()) {
 			cout << "Could not open or find the image!\n" << endl;
 			cout << path1 + imgs[i] + ".jpg";
@@ -55,8 +62,10 @@ int main()
 				cout << path2 + vid[i] + ".mp4";
 				return -1;
 			}
-			cout << path2 + vid[i] + ".mp4";
-			Ptr<AKAZE> detector = AKAZE::create();
+			cout << path2 + vid[i] + ".mp4" <<endl;
+
+			Ptr<AKAZE> detector = AKAZE::create(AKAZE::DESCRIPTOR_KAZE,64,3,0.0011f, 5, 5,KAZE::DIFF_PM_G1);
+			//Ptr<AKAZE> detector = AKAZE::create();
 			vector<KeyPoint> kp1, kp2;
 			Mat des1, des2, img_matches;
 			detector->detectAndCompute(img, noArray(), kp1, des1);
@@ -66,42 +75,48 @@ int main()
 				cap >> frame;
 				if (frame.empty())
 					break;
-				cvtColor(frame, gray, COLOR_BGR2GRAY);
+				//cvtColor(frame, gray, COLOR_BGR2GRAY);
 				
 
 
-				resize(gray, gray, cv::Size(300, 300), 0.75, 0.75);
-				detector->detectAndCompute(gray, noArray(), kp2, des2);
+				resize(frame, frame, cv::Size(300, 300), 0.75, 0.75);
+				detector->detectAndCompute(frame, noArray(), kp2, des2);
 				try{
 					Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE_HAMMING);
 					vector<vector <DMatch>> matches;
 					vector <DMatch>good_matches;
 					matcher->knnMatch(des1, des2, matches,2);
 					//-- Filter matches using the Lowe's ratio test
-					const float ratio_thresh = 0.7f;
+					const float ratio_thresh = 0.85f;
 				    for (size_t i = 0; i < matches.size(); i++)
 					{
-						if (matches[i][0].distance < ratio_thresh * matches[i][1].distance)
-						{
-							good_matches.push_back(matches[i][0]);
+						if (matches.size() != 0) {
+							if (matches[i][0].distance < ratio_thresh * matches[i][1].distance)
+							{
+								good_matches.push_back(matches[i][0]);
+							}
 						}
 					}
 
-					float percent = ((int)good_matches.size() / ((int)des1.rows* (int)des1.cols))*100;
+					float percent = (((float)good_matches.size() / (float)des1.cols) * (float)100);
 					cout << percent << endl;
-					drawMatches(img, kp1, gray, kp2, good_matches, img_matches, Scalar::all(-1),
+					drawMatches(img, kp1, frame, kp2, good_matches, img_matches, Scalar::all(-1),
 						Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 				}
 				catch (int e) {
 
 				}
+				stringstream ss;
+				ss << nr;
+				ss >> nrs;
+				imwrite("img_"+ nrs +".jpg", img_matches);
 					//-- Show detected matches
 				imshow("Matches", img_matches);
 				waitKey(25);
+				nr++;
 			}
 		}
 	}
-
 	//-- Draw matches
 
 	return 0;
